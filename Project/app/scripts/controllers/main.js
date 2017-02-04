87e362725d5128790c077ce1affac7492c7011c4
Dennis Durairaj <dennis.durairaj@gmail.com>
2017-02-04 15:30:20 +0100

    added ability to preview pdf before downloading

--- Project/app/scripts/controllers/main.js
+++ Project/app/scripts/controllers/main.js
@@ -10,11 +10,24 @@
 angular.module('projectApp')
   .controller('MainCtrl', function () {
     var vm = this;
-
+    vm.name, vm.email, vm.address = '';
     vm.generatePDF = function () {
-      console.log('clicked');
-      var doc = new jsPDF();
-      doc.text(vm.name, 10, 10);
-      doc.save('a4.pdf');
+        if(vm.email && vm.name) {
+            vm.doc = new jsPDF();
+            vm.doc.text('Name: ' + vm.name, 10, 10);
+            vm.doc.text('Email: ' + vm.email, 10, 20);
+            vm.doc.text('Address: ' + vm.address, 10, 30);
+            var pdf = vm.doc.output('datauristring');
+            $('iframe').attr('src', pdf);
+        }
+        else {
+            vm.error = true;
+        }
+    };
+
+    vm.downloadPDF = function () {
+        if(vm.doc) {
+            vm.doc.save('yourPDF.pdf');
+        }
     };
   });
